17ebc58ac41a8f7bb02e293424d06af67c0494bf
tedbu <124395912+tedbu@users.noreply.github.com>
2024-07-31 15:58:28 +0200

    Přidal jsem otačecí lejzr a obrazky.

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     wuKong.setLightMode(wuKong.LightMode.BREATH)
     wuKong.setAllMotor(50, 50)
+    basic.showIcon(IconNames.Ghost)
 })
 input.onButtonPressed(Button.AB, function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 360)
+    basic.pause(1000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
     wuKong.stopAllMotor()
     basic.showIcon(IconNames.Skull)
     wuKong.setLightMode(wuKong.LightMode.OFF)
@@ -10,6 +14,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     wuKong.setLightMode(wuKong.LightMode.BREATH)
     wuKong.setAllMotor(100, 100)
+    basic.showIcon(IconNames.Duck)
 })
 basic.showLeds(`
     . . . # #
